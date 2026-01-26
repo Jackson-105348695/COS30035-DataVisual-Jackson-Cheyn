0a5e16c06faae79f8fc6d6069c40e51f87a432f7
Jackson-105348695 <105348695@student.swin.edu.au>
2026-01-26 17:46:13 +1100

    Add files via upload

---
+++ sepsisJS.js
@@ -0,0 +1,98 @@
+//Version 3
+//Added axis
+
+//Setting width and hight variables
+var w = 1200;
+var h = 300;
+
+//Imports csv data defining 3 pieces of data then loading it into the variable dataset
+d3.csv("sepsis_30day_data.csv", function(d)
+{
+    return {
+        Country: d.Reference,
+        date: +d.TIME_PERIOD,
+        number: +d.OBS_VALUE
+    };
+}).then(function(data)
+{
+    dataset = data;
+    barChart(dataset);
+});
+
+function barChart(data)
+{
+    //Creating and loading data onto the x axis and specifying spacing
+    var xscale = d3.scaleBand()
+                    .domain(d3.range(data.length))
+                    .rangeRound([0,w])
+                    .paddingInner(0.05);
+
+    //Loads data onto y scale
+    var yscale = d3.scaleLinear() 
+                    .domain([0, d3.max(data, d => d.number)]) 
+                    .range([h, 0]);
+
+    //Formats x axis
+    var xaxis = d3.axisBottom()
+                .ticks(5)
+                .scale(xscale);
+
+    //Formats y axis
+    var yaxis = d3.axisLeft()
+                .ticks(5)
+                .scale(yscale);
+
+    //Creates svg onto webpage
+    var svg = d3.select("#chart")
+                    .append("svg")
+                    .attr("width", w +60) //+60 gives space for the axis
+                    .attr("height", h + 40) //+40 gives space for axis
+
+    //Creates x axis
+    svg.append("g")
+        .attr("transform", "translate(40, "+ (h +10) +")")
+        .call(xaxis);
+
+    //Creates y axis
+    svg.append("g")
+        .attr("transform", "translate("+ (40) + ", 10)")
+        .call(yaxis);
+
+    //editing svg
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i)
+        {
+            return xscale(i) +40; //Applies x scale
+        })
+        .attr("y", function(d)
+        {
+            return h - yscale(d.number) +10; //Applies y scale
+        })
+        .attr("width", xscale.bandwidth()) //Adjust width of bars to fit all data in svg
+        .attr("height", function(d)
+        {
+            return yscale(d.number); //adjust hight of bars to fit in svg
+        })
+        .attr("fill", "black") //Sets colour of bars to black
+        .on('mouseover', function(event, d) //This function applies when you mouse is hovered over a specific bar in the graph
+        {
+            d3.select(this)
+                .attr("fill", "orange"); //Sets colour of bar to orange when hovering over
+                var box = this.getBBox()
+
+                svg.append("text") //Writes variable onto the bar as well as centres the text on the bar
+                    .attr("id", "tooltip")
+                    .attr("x", box.x + box.width / 2)
+                    .attr("y", box.y + 15)
+                    .text(d.number); 
+        })
+        .on('mouseout', function(d) //This function sets it so when the mouse is no longer hovering over the bar it will revert back to black and remove the text
+        {
+            d3.select(this)
+                .attr("fill", "black")
+                d3.select("#tooltip").remove();
+        })
+}
